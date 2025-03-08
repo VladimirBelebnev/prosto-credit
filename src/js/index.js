35ade88d9b45825343db708146ccd74b512e70ba
Temistakes <temistakes@yandex.ru>
2025-03-08 21:58:01 +0300

    Commit #6

--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-// Libraries
-
-import "./modules/sliders.js";
-import "./modules/pop-ups.js";
-import "./modules/modals.js";
-import "./modules/phone-mask.js";
-import "./modules/form-submit.js";
-import fslightbox from "fslightbox";
